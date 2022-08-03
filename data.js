aa1d0a80139c8978d7284a80d9ca287ba865f588
deepakexodia@gmail.com <deepakexodia@gmail.com>
2022-08-03 17:29:48 +0530

    changes for Fast Response

---
+++ data.js
@@ -0,0 +1,502 @@
+var allUserData =[
+	{
+		"name": "Chadwick Rush",
+		"date": "01-26-01",
+		"boolean": "Yes",
+		"address": "122-1421 Amet Rd.",
+		"region": "Gorontalo",
+		"city": "Tual",
+		"city1": "Pekanbaru",
+		"date1": "03-31-21"
+	},
+	{
+		"name": "Natalie Santos",
+		"date": "09-22-95",
+		"boolean": "Yes",
+		"address": "621-6208 Velit Avenue",
+		"region": "North Sulawesi",
+		"city": "Palu",
+		"city1": "Padang Panjang",
+		"date1": "10-02-19"
+	},
+	{
+		"name": "Andrew Woodard",
+		"date": "02-19-98",
+		"boolean": "Yes",
+		"address": "Ap #665-6024 Mollis St.",
+		"region": "Lampung",
+		"city": "Semarang",
+		"city1": "Pontianak",
+		"date1": "07-28-18"
+	},
+	{
+		"name": "Jonah Hinton",
+		"date": "02-26-95",
+		"boolean": "No",
+		"address": "Ap #144-5609 Gravida Rd.",
+		"region": "East Java",
+		"city": "Bandar Lampung",
+		"city1": "Manado",
+		"date1": "10-16-15"
+	},
+	{
+		"name": "Hayfa Villarreal",
+		"date": "09-01-98",
+		"boolean": "Yes",
+		"address": "181-8241 Rutrum Street",
+		"region": "Bangka Belitung Islands",
+		"city": "Banjarbaru",
+		"city1": "Samarinda",
+		"date1": "07-29-17"
+	},
+	{
+		"name": "Edan Barron",
+		"date": "05-18-01",
+		"boolean": "No",
+		"address": "695-3461 Odio. Road",
+		"region": "South Sumatra",
+		"city": "Depok",
+		"city1": "Semarang",
+		"date1": "07-26-16"
+	},
+	{
+		"name": "Dai Kelley",
+		"date": "06-27-99",
+		"boolean": "Yes",
+		"address": "1401 Cursus St.",
+		"region": "West Sumatra",
+		"city": "Pekanbaru",
+		"city1": "Mamuju",
+		"date1": "07-06-20"
+	},
+	{
+		"name": "Donna Blevins",
+		"date": "10-13-97",
+		"boolean": "No",
+		"address": "P.O. Box 389, 7930 Ut St.",
+		"region": "Papua",
+		"city": "Manokwari",
+		"city1": "Baubau",
+		"date1": "02-27-18"
+	},
+	{
+		"name": "Ivy Downs",
+		"date": "09-15-97",
+		"boolean": "No",
+		"address": "Ap #812-6859 Eu Street",
+		"region": "Bengkulu",
+		"city": "Cimahi",
+		"city1": "Binjai",
+		"date1": "07-27-19"
+	},
+	{
+		"name": "Preston Cleveland",
+		"date": "06-04-01",
+		"boolean": "Yes",
+		"address": "460-747 Duis Rd.",
+		"region": "West Nusa Tenggara",
+		"city": "Manado",
+		"city1": "Ternate",
+		"date1": "06-06-17"
+	},
+	{
+		"name": "Finn Phillips",
+		"date": "07-01-95",
+		"boolean": "No",
+		"address": "958-6467 Et St.",
+		"region": "Bengkulu",
+		"city": "Jayapura",
+		"city1": "Mamuju",
+		"date1": "05-29-17"
+	},
+	{
+		"name": "Gisela Joyner",
+		"date": "10-25-95",
+		"boolean": "No",
+		"address": "Ap #217-7317 Non St.",
+		"region": "Riau Islands",
+		"city": "Ambon",
+		"city1": "Jambi",
+		"date1": "11-30-17"
+	},
+	{
+		"name": "Rhoda Mccray",
+		"date": "05-24-98",
+		"boolean": "No",
+		"address": "1769 Ut Ave",
+		"region": "West Kalimantan",
+		"city": "Dumai",
+		"city1": "Palangka Raya",
+		"date1": "11-04-16"
+	},
+	{
+		"name": "Burton Callahan",
+		"date": "11-07-97",
+		"boolean": "No",
+		"address": "4438 Nunc Ave",
+		"region": "Bali",
+		"city": "Banda Aceh",
+		"city1": "Palu",
+		"date1": "02-21-18"
+	},
+	{
+		"name": "Aimee Perry",
+		"date": "08-13-00",
+		"boolean": "Yes",
+		"address": "2438 Morbi Road",
+		"region": "North Maluku",
+		"city": "Palangka Raya",
+		"city1": "Tanjung Pinang",
+		"date1": "05-08-19"
+	},
+	{
+		"name": "Berk Mack",
+		"date": "11-21-95",
+		"boolean": "No",
+		"address": "1731 Pharetra. Av.",
+		"region": "Jambi",
+		"city": "Ambon",
+		"city1": "Serang",
+		"date1": "10-01-18"
+	},
+	{
+		"name": "Lillith Shields",
+		"date": "07-07-97",
+		"boolean": "No",
+		"address": "P.O. Box 151, 9247 Ornare Road",
+		"region": "Special Region of Yogyakarta",
+		"city": "Pontianak",
+		"city1": "Bontang",
+		"date1": "08-18-17"
+	},
+	{
+		"name": "Palmer Bauer",
+		"date": "06-27-00",
+		"boolean": "No",
+		"address": "P.O. Box 522, 6504 Elit. Street",
+		"region": "Bali",
+		"city": "Gorontalo",
+		"city1": "Sungai Penuh",
+		"date1": "12-05-16"
+	},
+	{
+		"name": "Leila Hurst",
+		"date": "04-14-98",
+		"boolean": "Yes",
+		"address": "Ap #233-8021 Vitae St.",
+		"region": "Bali",
+		"city": "Samarinda",
+		"city1": "Manokwari",
+		"date1": "08-20-15"
+	},
+	{
+		"name": "Roth Walls",
+		"date": "04-10-98",
+		"boolean": "No",
+		"address": "373-1747 Ornare Road",
+		"region": "East Kalimantan",
+		"city": "Parepare",
+		"city1": "Semarang",
+		"date1": "04-26-21"
+	},
+	{
+		"name": "David Miranda",
+		"date": "03-20-95",
+		"boolean": "No",
+		"address": "P.O. Box 642, 4038 Nulla Street",
+		"region": "North Sumatra",
+		"city": "Parepare",
+		"city1": "Singkawang",
+		"date1": "10-28-15"
+	},
+	{
+		"name": "Kieran Mitchell",
+		"date": "06-28-99",
+		"boolean": "No",
+		"address": "Ap #383-8008 Id Avenue",
+		"region": "Banten",
+		"city": "Palembang",
+		"city1": "Makassar",
+		"date1": "06-02-20"
+	},
+	{
+		"name": "Ezra Hamilton",
+		"date": "06-19-99",
+		"boolean": "No",
+		"address": "Ap #396-3724 Ac Avenue",
+		"region": "Riau Islands",
+		"city": "Pangkalpinang",
+		"city1": "Gorontalo",
+		"date1": "10-13-18"
+	},
+	{
+		"name": "Scott Cooley",
+		"date": "09-30-98",
+		"boolean": "Yes",
+		"address": "946-3811 Pellentesque Avenue",
+		"region": "West Sumatra",
+		"city": "Madiun",
+		"city1": "Palopo",
+		"date1": "05-28-17"
+	},
+	{
+		"name": "Aaron Bates",
+		"date": "01-31-00",
+		"boolean": "Yes",
+		"address": "Ap #579-3599 Sollicitudin Street",
+		"region": "Special Region of Yogyakarta",
+		"city": "Mataram",
+		"city1": "Balikpapan",
+		"date1": "02-02-21"
+	},
+	{
+		"name": "Evan Brooks",
+		"date": "12-27-96",
+		"boolean": "Yes",
+		"address": "Ap #814-7555 Mollis Ave",
+		"region": "North Sumatra",
+		"city": "Mamuju",
+		"city1": "East Jakarta",
+		"date1": "10-28-20"
+	},
+	{
+		"name": "Armando Conway",
+		"date": "05-05-96",
+		"boolean": "No",
+		"address": "Ap #107-1834 Scelerisque Av.",
+		"region": "Bengkulu",
+		"city": "Manokwari",
+		"city1": "Bitung",
+		"date1": "11-11-20"
+	},
+	{
+		"name": "Kane Greene",
+		"date": "10-10-00",
+		"boolean": "Yes",
+		"address": "Ap #552-7273 Duis Road",
+		"region": "Special Region of Yogyakarta",
+		"city": "Manado",
+		"city1": "Gorontalo",
+		"date1": "01-18-17"
+	},
+	{
+		"name": "Camden Macdonald",
+		"date": "12-06-94",
+		"boolean": "Yes",
+		"address": "781-8055 Et Rd.",
+		"region": "North Maluku",
+		"city": "Banjarbaru",
+		"city1": "Mataram",
+		"date1": "04-09-16"
+	},
+	{
+		"name": "Phillip Frank",
+		"date": "10-18-94",
+		"boolean": "Yes",
+		"address": "881-2698 Integer Av.",
+		"region": "Papua",
+		"city": "Palangka Raya",
+		"city1": "Singkawang",
+		"date1": "07-11-17"
+	},
+	{
+		"name": "Cora Wong",
+		"date": "03-08-01",
+		"boolean": "Yes",
+		"address": "Ap #793-3593 Interdum. St.",
+		"region": "West Papua",
+		"city": "Cirebon",
+		"city1": "Yogyakarta",
+		"date1": "08-19-18"
+	},
+	{
+		"name": "Madonna Morrison",
+		"date": "09-17-99",
+		"boolean": "No",
+		"address": "8108 Aliquet Rd.",
+		"region": "West Sumatra",
+		"city": "Mamuju",
+		"city1": "Gunungsitoli",
+		"date1": "08-23-19"
+	},
+	{
+		"name": "Joan Snow",
+		"date": "02-11-95",
+		"boolean": "Yes",
+		"address": "971-6445 Magna Rd.",
+		"region": "North Sumatra",
+		"city": "Tidore",
+		"city1": "Sungai Penuh",
+		"date1": "06-04-21"
+	},
+	{
+		"name": "Pascale Massey",
+		"date": "11-11-00",
+		"boolean": "Yes",
+		"address": "797-9985 Amet Av.",
+		"region": "Maluku",
+		"city": "Pariaman",
+		"city1": "Pekanbaru",
+		"date1": "01-22-21"
+	},
+	{
+		"name": "Keegan Henry",
+		"date": "02-16-98",
+		"boolean": "Yes",
+		"address": "791-7710 Suspendisse St.",
+		"region": "North Sumatra",
+		"city": "Surabaya",
+		"city1": "Yogyakarta",
+		"date1": "01-29-19"
+	},
+	{
+		"name": "Zelda Shaw",
+		"date": "12-03-00",
+		"boolean": "No",
+		"address": "Ap #255-198 Leo. St.",
+		"region": "Central Java",
+		"city": "Bandar Lampung",
+		"city1": "Tanjungbalai",
+		"date1": "10-08-17"
+	},
+	{
+		"name": "Elton Bright",
+		"date": "12-12-98",
+		"boolean": "No",
+		"address": "846-7796 Etiam Street",
+		"region": "West Sulawesi",
+		"city": "Denpasar",
+		"city1": "Padang Sidempuan",
+		"date1": "10-09-16"
+	},
+	{
+		"name": "Unity Cobb",
+		"date": "01-29-00",
+		"boolean": "Yes",
+		"address": "Ap #187-1137 Senectus Rd.",
+		"region": "Bengkulu",
+		"city": "Pontianak",
+		"city1": "Kotamobagu",
+		"date1": "03-01-20"
+	},
+	{
+		"name": "Cleo Murphy",
+		"date": "02-20-00",
+		"boolean": "Yes",
+		"address": "P.O. Box 527, 6558 Nunc Road",
+		"region": "Gorontalo",
+		"city": "Jambi",
+		"city1": "Jayapura",
+		"date1": "07-15-19"
+	},
+	{
+		"name": "Berk Goodwin",
+		"date": "03-14-98",
+		"boolean": "No",
+		"address": "Ap #579-9518 Mauris Ave",
+		"region": "West Kalimantan",
+		"city": "Padang Sidempuan",
+		"city1": "Sungai Penuh",
+		"date1": "09-11-17"
+	},
+	{
+		"name": "Arthur Boyle",
+		"date": "06-04-96",
+		"boolean": "Yes",
+		"address": "P.O. Box 978, 5843 Volutpat. St.",
+		"region": "Riau",
+		"city": "Bontang",
+		"city1": "Ternate",
+		"date1": "12-05-19"
+	},
+	{
+		"name": "Daniel Foster",
+		"date": "07-19-99",
+		"boolean": "Yes",
+		"address": "6956 Nec Av.",
+		"region": "Central Kalimantan",
+		"city": "Bitung",
+		"city1": "Dumai",
+		"date1": "01-13-18"
+	},
+	{
+		"name": "Colorado Workman",
+		"date": "08-17-96",
+		"boolean": "No",
+		"address": "798-494 Fusce Rd.",
+		"region": "West Sumatra",
+		"city": "Gorontalo",
+		"city1": "Balikpapan",
+		"date1": "11-09-20"
+	},
+	{
+		"name": "Fiona Kane",
+		"date": "07-09-98",
+		"boolean": "No",
+		"address": "Ap #982-8165 Euismod Street",
+		"region": "Central Java",
+		"city": "Tidore",
+		"city1": "Tebing Tinggi",
+		"date1": "04-29-17"
+	},
+	{
+		"name": "Cruz Burns",
+		"date": "07-29-95",
+		"boolean": "No",
+		"address": "Ap #514-6389 At Street",
+		"region": "North Maluku",
+		"city": "Jayapura",
+		"city1": "Pasuruan",
+		"date1": "09-09-20"
+	},
+	{
+		"name": "Simon Michael",
+		"date": "05-21-00",
+		"boolean": "Yes",
+		"address": "5120 Gravida Rd.",
+		"region": "Aceh",
+		"city": "Pekanbaru",
+		"city1": "Mamuju",
+		"date1": "12-08-16"
+	},
+	{
+		"name": "Darryl Barrera",
+		"date": "07-12-01",
+		"boolean": "Yes",
+		"address": "431-1704 Tempor, Road",
+		"region": "Gorontalo",
+		"city": "Banjarbaru",
+		"city1": "Mataram",
+		"date1": "12-26-15"
+	},
+	{
+		"name": "Bevis Chambers",
+		"date": "05-16-95",
+		"boolean": "Yes",
+		"address": "626-350 Tellus Avenue",
+		"region": "Jambi",
+		"city": "Manokwari",
+		"city1": "Makassar",
+		"date1": "12-14-19"
+	},
+	{
+		"name": "Whilemina Wheeler",
+		"date": "10-27-94",
+		"boolean": "No",
+		"address": "Ap #697-7370 Et Av.",
+		"region": "Southeast Sulawesi",
+		"city": "Mamuju",
+		"city1": "Pangkalpinang",
+		"date1": "10-19-17"
+	},
+	{
+		"name": "Buffy Munoz",
+		"date": "09-01-95",
+		"boolean": "No",
+		"address": "537-9282 Semper Rd.",
+		"region": "Riau",
+		"city": "Yogyakarta",
+		"city1": "Kupang",
+		"date1": "07-21-19"
+	}
+]
